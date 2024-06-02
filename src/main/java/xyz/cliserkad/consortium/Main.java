efbc04e39732b4e77527344cef6e0e400bec5e47
cliserkad <0_0@cliserkad.xyz>
2024-06-02 18:26:33 -0400

    initial commit

---
+++ src/main/java/xyz/cliserkad/consortium/Main.java
@@ -0,0 +1,10 @@
+package xyz.cliserkad.consortium;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for(BoardPosition position : BoardPosition.values())
+			System.out.println(position);
+	}
+
+}
---
+++ src/main/java/xyz/cliserkad/consortium/Printer.java
@@ -0,0 +1,5 @@
+package xyz.cliserkad.consortium;
+
+public class Printer {
+
+}
