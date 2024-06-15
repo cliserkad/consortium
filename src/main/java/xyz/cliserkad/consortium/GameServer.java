ea17c37878a45a164d903dbd18b354f91816cde1
cliserkad <0_0@cliserkad.xyz>
2024-06-15 16:58:21 -0400

    fix turn delay. add launch options

--- src/main/java/xyz/cliserkad/consortium/GameServer.java
+++ src/main/java/xyz/cliserkad/consortium/GameServer.java
@@ -1,38 +1,52 @@
 package xyz.cliserkad.consortium;
 
-import javax.swing.*;
 import java.io.IOException;
-import java.util.TimerTask;
-import java.util.Timer;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Holds game state, manages the game loop, sends updates to clients and processes client input.
  */
-public class GameServer extends TimerTask {
+public class GameServer implements Runnable {
 	private GameState gameState;
-
 	private int turns;
 
-	public GameServer(int playerCount) throws IOException, InterruptedException {
-		NetworkServer server0 = new NetworkServer(5555);
-		NetworkServer server1 = new NetworkServer(5556);
-		server0.start();
-		server1.start();
-		server0.join();
-		server1.join();
-		Player p0 = new Player(server0);
-		Player p1 = new Player(server1);
+	public GameServer(int playerCount, int aiCount) throws IOException, InterruptedException {
+		List<Player> players = new ArrayList<>();
+		List<NetworkServer> servers = new ArrayList<>();
+
+		for(int i = 0; i < playerCount; i++) {
+			NetworkServer server = new NetworkServer(5555 + i);
+			server.start();
+			servers.add(server);
+		}
+
+		for(int i = 0; i < aiCount; i++) {
+			players.add(new Player(new AutoGameClient()));
+		}
+
+		for(NetworkServer server : servers) {
+			server.join();
+		}
+
+		for(NetworkServer server : servers) {
+			players.add(new Player(server));
+		}
 
 		System.err.println("Connections accepted...");
-
-		Player[] players = { p0, p1 };
-		gameState = new GameState(players);
+		gameState = new GameState(players.toArray(new Player[playerCount + aiCount]));
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		GameServer server = new GameServer(3);
-		Timer timer = new Timer("GameServerTimer");
-		timer.scheduleAtFixedRate(server, 3000, 3000);
+		GameServer server = new GameServer(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+		while(true) {
+			try {
+				server.run();
+				Thread.sleep(2000);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
