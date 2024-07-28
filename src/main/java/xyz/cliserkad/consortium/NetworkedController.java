c9c0ba0a34950b30d82bb3a12780350e154f1778
cliserkad <0_0@cliserkad.xyz>
2024-07-28 11:18:16 -0400

    add exception handling to NetworkedController

--- src/main/java/xyz/cliserkad/consortium/NetworkedController.java
+++ src/main/java/xyz/cliserkad/consortium/NetworkedController.java
@@ -1,8 +1,6 @@
 package xyz.cliserkad.consortium;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -16,11 +14,13 @@ public class NetworkedController<InterfaceClass> extends Thread implements Invoc
 
 	public static final boolean DEFAULT_IS_VERBOSE = false;
 
-	public InterfaceClass proxy;
+	public final InterfaceClass proxy;
+	public final int port;
+
 	/**
 	 * The network connection to coordinating server
 	 */
-	private final ServerSocket serverSocket;
+	private ServerSocket serverSocket;
 	private Socket clientSocket;
 
 	private ObjectInputStream in;
@@ -31,7 +31,7 @@ public class NetworkedController<InterfaceClass> extends Thread implements Invoc
 	 * Initializes a new networked controller, against which all methods supplied by InterfaceClass can be invoked. The controller will listen on the specified port for incoming connections.
 	 */
 	public NetworkedController(final int port, final Class<InterfaceClass> interfaceClass, final boolean isVerbose) throws IOException {
-		serverSocket = new ServerSocket(port);
+		this.port = port;
 		this.isVerbose = isVerbose;
 
 		// trust that the standard library actually works
@@ -44,7 +44,14 @@ public class NetworkedController<InterfaceClass> extends Thread implements Invoc
 
 	@Override
 	public void start() {
+		start0();
+	}
+
+	private void start0() {
 		try {
+			if(serverSocket != null)
+				serverSocket.close();
+			serverSocket = new ServerSocket(port);
 			if(isVerbose)
 				System.out.println("Server started and listening on port " + serverSocket.getLocalPort());
 			clientSocket = serverSocket.accept();
@@ -53,19 +60,124 @@ public class NetworkedController<InterfaceClass> extends Thread implements Invoc
 			out = new ObjectOutputStream(clientSocket.getOutputStream());
 			in = new ObjectInputStream(clientSocket.getInputStream());
 		} catch(IOException e) {
-			e.printStackTrace();
-			return;
+			if(isVerbose) {
+				System.err.println("NetworkedController encountered an IOException. Will retry connection...");
+				e.printStackTrace();
+			}
+			sleep();
+			start0();
 		}
 	}
 
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		out.reset();
-		for(Object arg : args) {
-			out.writeObject(arg);
+		return invoke0(proxy, method, args, 0);
+	}
+
+	private Object invoke0(Object proxy, Method method, Object[] args, int callNum) throws Throwable {
+		if(callNum != 0)
+			sleep();
+
+		if(callNum > 3) {
+			if(isVerbose)
+				System.err.println("Multiple IOExceptions encountered. Attempting to restart connection...");
+			start0();
 		}
-		out.writeObject(new MethodInvocation(method.getName()));
-		return in.readObject();
+
+		try {
+			out.reset();
+		} catch(IOException e) {
+			if(isVerbose) {
+				System.err.println("NetworkedController encountered an IOException. Will retry invocation...");
+				e.printStackTrace();
+			}
+			return invoke0(proxy, method, args, ++callNum);
+		}
+
+		try {
+			for(Object arg : args) {
+				out.writeObject(arg);
+			}
+		} catch(InvalidClassException invalidClassException) {
+			if(isVerbose) {
+				System.err.println("FATAL: NetworkedController can't write out given class.");
+				invalidClassException.printStackTrace();
+			}
+			throw invalidClassException;
+		} catch(NotSerializableException notSerializableException) {
+			if(isVerbose) {
+				System.err.println("FATAL: NetworkedController can't serialize given object.");
+				notSerializableException.printStackTrace();
+			}
+			throw notSerializableException;
+		} catch(IOException ioException) {
+			if(isVerbose) {
+				System.err.println("NetworkedController encountered an IOException. Will retry invocation...");
+				ioException.printStackTrace();
+			}
+			return invoke0(proxy, method, args, ++callNum);
+		}
+
+		try {
+			out.writeObject(new MethodInvocation(method.getName()));
+		} catch(InvalidClassException | NotSerializableException invalidClassException) {
+			if(isVerbose) {
+				System.err.println("FATAL: NetworkedController can't serialize the MethodInvocation object.");
+				invalidClassException.printStackTrace();
+			}
+			throw invalidClassException;
+		} catch(IOException ioException) {
+			if(isVerbose) {
+				System.err.println("NetworkedController encountered an IOException. Will retry invocation...");
+				ioException.printStackTrace();
+			}
+			return invoke0(proxy, method, args, ++callNum);
+		}
+
+		try {
+			return in.readObject();
+		} catch(ClassNotFoundException classNotFoundException) {
+			if(isVerbose) {
+				System.err.println("FATAL: NetworkedController can't load class for object received from client/NetworkedResponder");
+				classNotFoundException.printStackTrace();
+			}
+			throw classNotFoundException;
+		} catch(InvalidClassException invalidClassException) {
+			if(isVerbose) {
+				System.err.println("FATAL: NetworkedController can't read in class from client/NetworkedResponder.");
+				invalidClassException.printStackTrace();
+			}
+			throw invalidClassException;
+		} catch(StreamCorruptedException streamCorruptedException) {
+			if(isVerbose) {
+				System.err.println("NetworkedController encountered a StreamCorruptedException. Will retry invocation...");
+				streamCorruptedException.printStackTrace();
+			}
+			return invoke0(proxy, method, args, ++callNum);
+		} catch(OptionalDataException optionalDataException) {
+			if(isVerbose) {
+				System.err.println("FATAL: NetworkedController received primitive data types instead of objects. The client/NetworkedResponder needs to wrap primitives.");
+				optionalDataException.printStackTrace();
+			}
+			throw optionalDataException;
+		} catch(IOException ioException) {
+			if(isVerbose) {
+				System.err.println("NetworkedController encountered an IOException. Will retry invocation...");
+				ioException.printStackTrace();
+			}
+			return invoke0(proxy, method, args, ++callNum);
+		}
+	}
+
+	private void sleep() {
+		try {
+			Thread.sleep(1000);
+		} catch(InterruptedException e) {
+			if(isVerbose) {
+				e.printStackTrace();
+				System.err.println("Sleep interrupted, continuing...");
+			}
+		}
 	}
 
 }
