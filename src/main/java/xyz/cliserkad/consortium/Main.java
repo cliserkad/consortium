ea59c24a05638c7d78f302c85a1e1ffde76a376a
cliserkad <0_0@cliserkad.xyz>
2025-03-08 20:23:28 -0500

    simplify version reporting

--- src/main/java/xyz/cliserkad/consortium/Main.java
+++ src/main/java/xyz/cliserkad/consortium/Main.java
@@ -1,19 +1,11 @@
 package xyz.cliserkad.consortium;
 
 import java.awt.*;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.JarURLConnection;
 import java.net.URL;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 public class Main {
 
@@ -39,9 +31,7 @@ public class Main {
 	public static final Random RANDOM = new Random();
 	public static final ResourcesDescriptor RESOURCES = describeResources();
 	public static final float INITIAL_WINDOW_SIZE_FACTOR = 0.8f;
-	public static final String GIT_INFO = "git.properties";
-	public static final String VERSION = version();
-	public static final String TITLE = "Consortium " + VERSION;
+	public static final String TITLE = "Consortium " + Version.COMMIT_ID_ABBREV;
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		if(args.length == 2) {
@@ -83,59 +73,6 @@ public class Main {
 			throw new IllegalStateException("Unsupported URL protocol: " + classURL.getProtocol());
 		}
 		return new ResourcesDescriptor(root, jarFile);
-	}
-
-	public static String version() {
-		String commitId = "COMMIT_ID_UNKNOWN";
-		if(RESOURCES.jarFile != null) {
-			try {
-				String result = new BufferedReader(new InputStreamReader(resourcesAt(GIT_INFO).getFirst().openStream())).lines().collect(Collectors.joining("\n"));
-				for(String line : result.split("\n")) {
-					commitId = updateVersionIfInString(commitId, line);
-				}
-			} catch(IOException e) {
-				e.printStackTrace();
-			}
-		} else {
-			File gitProperties = new File(RESOURCES.resourcesRoot, GIT_INFO);
-			if(gitProperties.exists()) {
-				try {
-					List<String> lines = Files.readAllLines(gitProperties.toPath());
-					for(String line : lines) {
-						commitId = updateVersionIfInString(commitId, line);
-					}
-				} catch(IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return commitId;
-	}
-
-	public static String updateVersionIfInString(String version, String line) {
-		if(line.startsWith("git.commit.id.abbrev")) {
-			return line.split("=")[1];
-		} else {
-			return version;
-		}
-	}
-
-	public static List<URL> resourcesAt(String prefix) throws IOException {
-		ZipInputStream zip = null;
-		zip = new ZipInputStream(RESOURCES.jarFile.toURI().toURL().openStream());
-		List<URL> resources = new ArrayList<>();
-		while(true) {
-			ZipEntry entry = zip.getNextEntry();
-			if(entry == null) {
-				break;
-			}
-			final String name = entry.getName();
-			if(name.startsWith(prefix) && !entry.isDirectory()) {
-				resources.add(Main.class.getClassLoader().getResource(name));
-			}
-		}
-		zip.close();
-		return resources;
 	}
 
 	public static String prettifyEnumName(String input) {
