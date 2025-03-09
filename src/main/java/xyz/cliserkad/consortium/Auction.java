402f9c05f9730ad3db6230e800bdb570604143aa
cliserkad <0_0@cliserkad.xyz>
2025-03-09 14:02:35 -0400

    increase starting bids

--- src/main/java/xyz/cliserkad/consortium/Auction.java
+++ src/main/java/xyz/cliserkad/consortium/Auction.java
@@ -4,12 +4,11 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class Auction implements Serializable {
 
 	@Serial
-	private static final long serialVersionUID = 20240714;
+	private static final long serialVersionUID = 20250309;
 
 	public final BoardElement property;
 	public final List<Player> bidders;
@@ -17,11 +16,16 @@ public class Auction implements Serializable {
 	public List<BidAction> bids;
 	public int bid;
 
+	// TODO: fix purchasable interface
 	public Auction(BoardElement property, List<Player> bidders) {
 		this.property = property;
 		this.bidders = bidders;
 		bids = new ArrayList<>();
-		bid = 0;
+		if(property instanceof Purchasable purchasable) {
+			bid = (int) (purchasable.cost() * 0.25f);
+		} else {
+			bid = 0;
+		}
 	}
 
 }
